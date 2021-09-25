ffda290e5a20421b284c29fa2ff1bb974b467a88
Patetico <pat@pateti.com>
2021-09-24 23:14:22 -0400

    Create main package and Token class

---
+++ compilador/token.py
@@ -0,0 +1,17 @@
+from enum import auto, Enum
+
+
+class TokenType(Enum):
+    IDENTIFICADOR = auto()
+    INTEIRO = auto()
+    REAL = auto()
+    SIMBOLO = auto()
+
+
+class Token:
+    def __init__(self, tipo: TokenType, valor: str):
+        self.tipo = tipo
+        self.valor = valor
+
+    def __str__(self):
+        return f"Token<{self.tipo}, {self.valor}>"
