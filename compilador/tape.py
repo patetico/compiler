fba5ecec48f3d34bdfdc8e61445451b9d7d4dd4e
Patetico <pat@pateti.com>
2021-09-26 01:14:27 -0400

    Refactor tape logic

--- compilador/tape.py
+++ compilador/tape.py
@@ -5,29 +5,39 @@ class Tape:
         self.pos = 0
 
     def next(self):
-        if len(self.code) > self.pos:
+        if self.is_eof(1):
             raise EOFError()
         self.pos += 1
 
     def back(self):
-        if self.pos < 0:
+        if self.is_eof(-1):
             raise EOFError()
         self.pos -= 1
 
     def get_char(self, pos=0):
         pos += self.pos
-        if pos >= len(self.code) or pos < 0:
+        if self.is_eof():
             raise EOFError()
         return self.code[pos]
 
     def is_letra(self, pos=0):
+        if self.is_eof(pos):
+            return False
         c = self.get_char(pos)
         return ('a' <= c <= 'z') or ('A' <= c <= 'Z')
 
     def is_num(self, pos=0):
+        if self.is_eof(pos):
+            return False
         c = self.get_char(pos)
         return '0' <= c <= '9'
 
     def is_whitespace(self, pos=0):
+        if self.is_eof(pos):
+            return False
         c = self.get_char(pos)
         return c in ' \r\n\t'
+
+    def is_eof(self, pos=0):
+        pos += self.pos
+        return pos >= len(self.code) or pos < 0
