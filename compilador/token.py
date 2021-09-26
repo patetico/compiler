f8a825c57a6afad322a192445d34f0f9a984e151
Patetico <pat@pateti.com>
2021-09-26 15:24:13 -0400

    Create Token.__eq__()

--- compilador/token.py
+++ compilador/token.py
@@ -15,4 +15,13 @@ class Token:
         self.valor = valor
 
     def __str__(self):
-        return f"Token<{self.tipo}, {self.valor}>"
+        return f"Token<{self.tipo}, {self.valor!r}>"
+
+    def __eq__(self, other):
+        if isinstance(other, Token):
+            return self.tipo == other.tipo and self.valor == other.valor
+        elif (isinstance(other, tuple) or isinstance(other, list)) and len(other) == 2:
+            tipo, valor = other
+            return self.tipo == tipo and self.valor == valor
+
+        return False
