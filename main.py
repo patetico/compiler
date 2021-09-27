95d8f8c1776348cf785cf17158147c1c58b62714
Patetico <pat@pateti.com>
2021-09-27 18:23:05 -0400

    Refactor main.py

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import sys
+
+from compilador.lexicon.lexicon import Lexicon
+
+
 if __name__ == '__main__':
-    print('Hello world')
+    if len(sys.argv) < 2:
+        filepath = 'dados/exemplo.lalg.txt'
+    else:
+        filepath = sys.argv[1]
+
+    code = Lexicon(filepath).parse()
+
+    print('Finished parsing!')
+    print('Generated code:\n')
+    print('\n'.join(f'{line:>5} | {code}' for line, code in enumerate(code)))
