50f2c39a1173215275d92d9edf08a2da5ecd6a79
Patetico <pat@pateti.com>
2021-09-27 02:02:12 -0400

    Improve SymbolsTable

--- compilador/symbols_table.py
+++ compilador/symbols_table.py
@@ -2,33 +2,72 @@ from compilador.errors import CompilerSemanticError
 from compilador.token import Token, TokenType
 
 
+def normalize_name(name):
+    if isinstance(name, Token):
+        name = name.valor
+    elif isinstance(name, Symbol):
+        name = name.name
+
+    if not isinstance(name, str):
+        raise CompilerSemanticError(f'Nome inválido de símbolo: {name!r}')
+
+    return name
+
+
+class Symbol:
+    def __init__(self, name, type_: TokenType):
+        self.name = normalize_name(name)
+        self.type = type_
+
+    def __str__(self):
+        return repr(self.name)
+
+    def __repr__(self):
+        return f'Symbol<{self.type}, {self.name}>'
+
+    def same_type(self, other):
+        if not isinstance(other, Symbol):
+            return self.type == other
+        return self.type == other.type
+
+
 class SymbolsTable:
     def __init__(self):
         self.symbols = dict()
         self._tmp_id = 1
 
-    def has(self, name: str):
-        return name in self.symbols
+    def get(self, name) -> Symbol:
+        name = normalize_name(name)
+        return self.symbols[name]
+
+    def has(self, name):
+        return normalize_name(name) in self.symbols
 
     def add(self, name: Token, type_: TokenType):
         if name.tipo != TokenType.IDENTIFICADOR:
             raise CompilerSemanticError(
                 f'Tentativa de criar variável com identicador inválido {name!r}')
 
-        if self.has(name.valor):
-            raise CompilerSemanticError(
-                f'Redeclaração de variável {name!r}')
-
         if type_ not in (TokenType.REAL, TokenType.INTEIRO):
             raise CompilerSemanticError(
                 f'Tentativa de criar variável {name.valor!r} com tipo inválido {type_}')
 
-        self.symbols[name.valor] = type_
+        name = normalize_name(name)
+        if self.has(name):
+            raise CompilerSemanticError(f'Redeclaração de variável {name!r}')
 
-    def _make_temp(self, type_: TokenType) -> Token:
+        self.symbols[name] = Symbol(name, type_)
+
+    def make_temp(self, type_: TokenType) -> Token:
         while self.has(f't{self._tmp_id}'):
             self._tmp_id += 1
 
         tmp = Token.identificador(f't{self._tmp_id}')
         self.add(tmp, type_)
         return tmp
+
+    def typeof(self, name):
+        return self.get(name).type
+
+    def same_types(self, var1, var2):
+        return self.typeof(var1) == self.typeof(var2)
