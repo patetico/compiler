052daf3c73f4de7f37bd3e6da5585ea5a402d350
Patetico <pat@pateti.com>
2021-09-27 02:01:58 -0400

    Improve codegen code

--- compilador/codegen.py
+++ compilador/codegen.py
@@ -1,10 +1,23 @@
 import logging
 
+from .symbols_table import normalize_name
+from .token import Token
+
 
 _logger = logging.getLogger(__name__)
 
 
+def _reduce_to_arg(var) -> str:
+    if isinstance(var, Token) and var.is_number:
+        return var.valor
+
+    return normalize_name(var)
+
+
 def base(op, arg1='', arg2='', res=''):
+    arg1 = _reduce_to_arg(arg1)
+    arg2 = _reduce_to_arg(arg2)
+    res = _reduce_to_arg(res)
     code = f'{op};{arg1};{arg2};{res}'
     _logger.debug(code)
     return code
